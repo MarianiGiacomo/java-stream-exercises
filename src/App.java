3c0929a922a48783242bdc7636207b872cd0d90d
Giacomo Mariani <giacomo.mariani@gmail.com>
2025-02-04 13:51:22 +0200

    exercise 1 QID 2.2023

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Stream a list of int primitives between the range of 0 (inclusive) and 5
+        // (exclusive). Calculate and
+        // output the average.
+        IntStream.range(0, 5).average().ifPresent(System.out::println);
     }
 }
